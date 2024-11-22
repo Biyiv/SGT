b1dac28ace4d595e59f332e004ae3f40eab1cede
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-22 15:37:31 +0100

    base du code SQL

---
+++ app/Database/ScriptSQL/SGT.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "Utilisateur" (
+  "username" varchar PRIMARY KEY,
+  "nom" varchar NOT NULL,
+  "prenom" varchar NOT NULL,
+  "mail" varchar NOT NULL,
+  "mdp" varchar NOT NULL
+);
+
+CREATE TABLE "Tache" (
+  "id" integer PRIMARY KEY,
+  "titre" varchar NOT NULL,
+  "description" text,
+  "echeance" datetime NOT NULL,
+  "priorite" varchar NOT NULL,
+  "commentaire" Commentaire,
+  "creePar" Utilisateur NOT NULL,
+  "dateCreation" datetime NOT NULL
+);
+
+CREATE TABLE "Commentaire" (
+  "id" integer PRIMARY KEY,
+  "commentaire" text NOT NULL,
+  "tache" Tache NOT NULL,
+  "dateCreation" datetime DEFAULT 'CURRENT_TIMESTAMP'
+);
+
+ALTER TABLE "Commentaire" ADD FOREIGN KEY ("tache") REFERENCES "Tache" ("commentaire");
+
+ALTER TABLE "Tache" ADD FOREIGN KEY ("creePar") REFERENCES "Utilisateur" ("username");
