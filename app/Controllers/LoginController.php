63a6f204be7e2d279736bd645667bf54101d9bbc
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-22 16:01:33 +0100

    Création du controller permettant la connexion

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UtilisateurModel;
+
+class LoginController extends BaseController {
+	public function index() {
+		return view('login');
+	}
+
+	public function login() {
+		$utilisateurModel = new UtilisateurModel();
+		$utilisateur = $utilisateurModel->where('email', $this->request->getVar('email'))->first();
+
+		if ($utilisateur) {
+			if (password_verify($this->request->getVar('password'), $utilisateur['password'])) {
+				session()->set('utilisateur', $utilisateur);
+				return redirect()->to('/home');
+			} else {
+				session()->setFlashdata('error', 'Mot de passe incorrect');
+				return redirect()->to('/login');
+			}
+		} else {
+			return redirect()->to('/login');
+		}
+	}
+
+	public function register() {
+		$utilisateurModel = new UtilisateurModel();
+		$utilisateur = [
+			'username' => $this->request->getVar('username'),
+			'nom' => $this->request->getVar('nom'),
+			'prenom' => $this->request->getVar('prenom'),
+			'email' => $this->request->getVar('email'),
+			'mdp' => password_hash($this->request->getVar('mdp'), PASSWORD_DEFAULT)
+		];
+
+		$utilisateurModel->insert($utilisateur);
+		return redirect()->to('/login');
+	}
+
+	public function logout() {
+		session()->remove('utilisateur');
+		return redirect()->to('/login');
+	}
+
+}
