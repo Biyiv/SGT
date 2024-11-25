509596298d4b712b1540d58f88910a8b70a3643d
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-25 16:34:07 +0100

    Ajout des modals

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -41,6 +41,52 @@
 	<p>Aucune tache trouvé.</p>
 <?php endif; ?>
 
+
+<div id="creationTache">
+	<?= form_open('/creationTache'); ?>
+	
+	<?= form_label('Titre :', 'titre'); ?>
+	<?= form_input('titre', '', ['id' => 'titre']); ?>
+	<br>
+	<?= form_label('Description :', 'description'); ?>
+	<?= form_textarea('description', '', ['id' => 'description']); ?>
+	<br>
+	<?= form_label('Début :', 'debut'); ?>
+	<?= form_input(['type' => 'date', 'name' => 'debut', 'id' => 'debut']); ?>
+	<br>
+	<?= form_label('Echéance :', 'echeance'); ?>
+	<?= form_input(['type' => 'date', 'name' => 'echeance', 'id' => 'echeance']); ?>
+	<br>
+	<?= form_label('Priorité :', 'priorite'); ?>
+	<?= form_dropdown('priorite', ['1' => 'Faible', '2' => 'Moyenne', '3' => 'Importante'], '1', ['id' => 'priorite']); ?>
+	<br>
+	<?= form_submit('submit', 'Créer la tâche'); ?>
+	<?= form_close(); ?>
+
+</div>
+
+<div id="modificationProfil">
+	<?= 
+	$utilisateur = session()->get('utilisateur');?>
+
+	<?= form_open('/modificationProfil'); ?>
+	<?= form_label('Nom :', 'nom'); ?>
+	<?= form_input('nom', $utilisateur['nom'], ['id' => 'nom']); ?>
+	<br>
+	<?= form_label('Prénom :', 'prenom'); ?>
+	<?= form_input('prenom', $utilisateur['prenom'], ['id' => 'prenom']); ?>
+	<br>
+	<?= form_label('Email :', 'email'); ?>
+	<?= form_input('email', $utilisateur['email'], ['id' => 'email']); ?>
+	<br>
+	<?= form_label('Mot de passe :', 'motdepasse'); ?>
+	<?= form_input('motdepasse', '', ['id' => 'motdepasse']); ?>
+	<br>
+	<?= form_submit('submit', 'Modifier le profil'); ?>
+	<?= form_close(); ?>
+
+</div>
+
 </body>
 
 </html>
