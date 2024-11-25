441a86d2e5ab466ae2e50336833384f74e063a35
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-25 10:13:24 +0100

    indentation

--- app/Database/Migrations/2024-11-22-155148_UpdateCommentaireTable.php
+++ app/Database/Migrations/2024-11-22-155148_UpdateCommentaireTable.php
@@ -6,13 +6,13 @@ use CodeIgniter\Database\Migration;
 
 class UpdateCommentaireTable extends Migration
 {
-    public function up()
-    {
-		$this->db->query("ALTER TABLE commentaire ALTER COLUMN datecreation SET DEFAULT CURRENT_TIMESTAMP"); 
-    }
+	public function up()
+	{
+		$this->db->query("ALTER TABLE commentaire ALTER COLUMN datecreation SET DEFAULT CURRENT_TIMESTAMP");
+	}
 
-    public function down()
-    {
+	public function down()
+	{
 		$this->db->query("ALTER TABLE commentaire ALTER COLUMN datecreation SET DEFAULT NULL");
-    }
+	}
 }
