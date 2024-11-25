974fe1f39f167b586aebb30838e4120aca462953
Mathys <poretmathys2004@gmail.com>
2024-11-25 17:03:34 +0100

    Implémentation style sur le menu

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -4,6 +4,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Menu</title>
+
+	<link rel="stylesheet" href="/assets/css/stlMenu.css">
 </head>
 <h1>Liste des taches</h1>
 
@@ -41,51 +43,44 @@
 	<p>Aucune tache trouvé.</p>
 <?php endif; ?>
 
+<!-- Modal Trigger Button (optionnel, si tu veux un bouton pour ouvrir le modal) -->
+<button id="openModalBtn">Créer une Tâche</button>
 
-<div id="creationTache">
-	<?= form_open('/creationTache'); ?>
-	
-	<?= form_label('Titre :', 'titre'); ?>
-	<?= form_input('titre', '', ['id' => 'titre']); ?>
-	<br>
-	<?= form_label('Description :', 'description'); ?>
-	<?= form_textarea('description', '', ['id' => 'description']); ?>
-	<br>
-	<?= form_label('Début :', 'debut'); ?>
-	<?= form_input(['type' => 'date', 'name' => 'debut', 'id' => 'debut']); ?>
-	<br>
-	<?= form_label('Echéance :', 'echeance'); ?>
-	<?= form_input(['type' => 'date', 'name' => 'echeance', 'id' => 'echeance']); ?>
-	<br>
-	<?= form_label('Priorité :', 'priorite'); ?>
-	<?= form_dropdown('priorite', ['1' => 'Faible', '2' => 'Moyenne', '3' => 'Importante'], '1', ['id' => 'priorite']); ?>
-	<br>
-	<?= form_submit('submit', 'Créer la tâche'); ?>
-	<?= form_close(); ?>
-
+<!-- Modal -->
+<div id="creationTacheModal" class="modal">
+    <div class="modal-content">
+        <span class="close-btn" id="closeModalBtn">&times;</span>
+        
+        <!-- Formulaire de création de tâche -->
+        <?= form_open('/creationTache'); ?>
+        
+        <?= form_label('Titre :', 'titre'); ?>
+        <?= form_input('titre', '', ['id' => 'titre']); ?>
+        <br>
+        
+        <?= form_label('Description :', 'description'); ?>
+        <?= form_textarea('description', '', ['id' => 'description']); ?>
+        <br>
+        
+        <?= form_label('Début :', 'debut'); ?>
+        <?= form_input(['type' => 'date', 'name' => 'debut', 'id' => 'debut']); ?>
+        <br>
+        
+        <?= form_label('Echéance :', 'echeance'); ?>
+        <?= form_input(['type' => 'date', 'name' => 'echeance', 'id' => 'echeance']); ?>
+        <br>
+        
+        <?= form_label('Priorité :', 'priorite'); ?>
+        <?= form_dropdown('priorite', ['1' => 'Faible', '2' => 'Moyenne', '3' => 'Importante'], '1', ['id' => 'priorite']); ?>
+        <br>
+        
+        <?= form_submit('submit', 'Créer la tâche'); ?>
+        <?= form_close(); ?>
+    </div>
 </div>
 
-<div id="modificationProfil">
-	<?= 
-	$utilisateur = session()->get('utilisateur');?>
-
-	<?= form_open('/modificationProfil'); ?>
-	<?= form_label('Nom :', 'nom'); ?>
-	<?= form_input('nom', $utilisateur['nom'], ['id' => 'nom']); ?>
-	<br>
-	<?= form_label('Prénom :', 'prenom'); ?>
-	<?= form_input('prenom', $utilisateur['prenom'], ['id' => 'prenom']); ?>
-	<br>
-	<?= form_label('Email :', 'email'); ?>
-	<?= form_input('email', $utilisateur['email'], ['id' => 'email']); ?>
-	<br>
-	<?= form_label('Mot de passe :', 'motdepasse'); ?>
-	<?= form_input('motdepasse', '', ['id' => 'motdepasse']); ?>
-	<br>
-	<?= form_submit('submit', 'Modifier le profil'); ?>
-	<?= form_close(); ?>
-
-</div>
+<!-- Script JS pour ouvrir et fermer le modal -->
+<script src="/assets/js/fctMenu.js"></script>
 
 </body>
 
