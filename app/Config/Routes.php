04ce5e602464cd30481897738c0eea2c0bd707b3
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-26 13:14:39 +0100

    Routes changées

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,13 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
 $routes->get('/', 'LoginController::login');
 
-$routes->get('/login', 'LoginController::login');
-$routes->post('/login', 'LoginController::login');
+$routes->match(['get', 'post'], '/login', 'LoginController::login');
+
 $routes->get('/register', 'LoginController::login');
 $routes->post('/register', 'LoginController::register');
 $routes->get('/logout', 'LoginController::logout');
 
-$routes->get('/dashboard', 'DashboardController::index');
+$routes->get('/dashboard', 'TacheController::index');
+$routes->match(['get', 'post'], '/setTriPreference','TacheController::setTriPreference');
+$routes->post( '/ajouterTache','TacheController::ajouterTache');
