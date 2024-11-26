f01ccc9ec42a0edffff50aaa297f6fceaf3b3769
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-26 15:44:48 +0100

    bandeau droite

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -114,6 +114,40 @@
 		</div>
 	</div>
 
+
+	<div id="bandeau-droit">
+		<button id="fermer-bandeau">Fermer</button>
+		<h2 id="bandeau-titre"></h2>
+		<p id="bandeau-description"></p>
+		<p><strong>Créé par :</strong> <span id="bandeau-creepar"></span></p>
+		<p><strong>Début :</strong> <span id="bandeau-debut"></span></p>
+		<p><strong>Échéance :</strong> <span id="bandeau-echeance"></span></p>
+		<p><strong>Priorité :</strong>
+			<span id="bandeau-priorite">
+				<select name="select-priorite" id="select-priorite">
+					<option value="faible">Faible</option>
+					<option value="moyenne">Moyenne</option>
+					<option value="importante">Importante</option>
+				</select>
+			</span>
+		</p>
+		<p><strong>Statut :</strong>
+			<span id="bandeau-statut">
+				<select name="select-statut" id="select-statut">
+					<option value="en attente">En attente</option>
+					<option value="en cours">En cours</option>
+					<option value="terminee">Terminée</option>
+				</select>
+			</span>
+		</p>
+
+		<div class="commentaire">
+
+		</div>
+	</div>
+
+
+
 	<!-- Script JS pour ouvrir et fermer le modal -->
 	<script src="/assets/js/fctMenu.js"></script>
 </body>
