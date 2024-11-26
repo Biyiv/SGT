787ea3ee47561dc59288582788a0de9bf72f2cbe
Baptiste Hay <baptiste.hay@etu.univ-lehavre.fr>
2024-11-26 11:38:09 +0100

    meilleure gestion des sessions

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -6,8 +6,10 @@ use App\Models\UtilisateurModel;
 
 class LoginController extends BaseController {
 
+	protected $session;
 	public function __construct() {
 		helper('form');
+		$this->session = session();
 	}
 
 	public function login() {
@@ -21,10 +23,10 @@ class LoginController extends BaseController {
 
 			if ($utilisateur) {
 				if (password_verify($this->request->getVar('password'), $utilisateur['mdp'])) {
-					session()->set('utilisateur', $utilisateur);
+					$this->session->set('utilisateur', $utilisateur);
 					return redirect()->to('/dashboard');
 				} else {
-					session()->setFlashdata('error', 'Mot de passe incorrect');
+					$this->session->setFlashdata('error', 'Mot de passe incorrect');
 
 					// Si l'utilisateur a coché la case "Se souvenir de moi", on garde l'identifiant en cookie
 					if($this->request->getVar('remember')) {
@@ -36,7 +38,7 @@ class LoginController extends BaseController {
 					return redirect()->to('/login');
 				}
 			} else {
-				session()->setFlashdata('error', 'Identifiant incorrect');
+				$this->session->setFlashdata('error', 'Identifiant incorrect');
 				return redirect()->to('/login');
 			}
 		} else {
@@ -49,25 +51,25 @@ class LoginController extends BaseController {
 
 		//Vérification de l'unicité du nom d'utilisateur
 		if ($utilisateurModel->where('username', $this->request->getVar('username'))->first() && $this->request->getVar('username') != "") {
-			session()->setFlashdata('error', 'Ce nom d\'utilisateur est déjà utilisé');
+			$this->session->setFlashdata('error', 'Ce nom d\'utilisateur est déjà utilisé');
 			return redirect()->to('/register');
 		}
 
 		//Vérification de l'unicité de l'adresse mail
 		if ($utilisateurModel->where('mail', $this->request->getVar('email'))->first() && $this->request->getVar('email') != "") {
-			session()->setFlashdata('error', 'Cette adresse mail est déjà utilisée');
+			$this->session->setFlashdata('error', 'Cette adresse mail est déjà utilisée');
 			return redirect()->to('/register');
 		}
 
 		// Vérification que le mot de passe fasse au moins 8 caractères, contienne une majuscule, une minuscule et un chiffre
 		if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', $this->request->getVar('mdp'))) {
-			session()->setFlashdata('error', 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre');
+			$this->session->setFlashdata('error', 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre');
 			return redirect()->to('/register');
 		}
 
 		//Vérification du mot de passe
 		if ($this->request->getVar('mdp') != $this->request->getVar('mdp_confirm')) {
-			session()->setFlashdata('error', 'Les mots de passe ne correspondent pas');
+			$this->session->setFlashdata('error', 'Les mots de passe ne correspondent pas');
 			return redirect()->to('/register');
 		}
 
@@ -109,13 +111,13 @@ class LoginController extends BaseController {
 				$emailService->setSubject('Réinitialisation du mot de passe');
 				$emailService->setMessage($message);
 				if ($emailService->send()) {
-					session()->setFlashdata('success', 'E-mail envoyé avec succès. (' . $mail . ')');
+					$this->session->setFlashdata('success', 'E-mail envoyé avec succès. (' . $mail . ')');
 					return redirect()->to('/forgotpwd');
 				} else {
 					echo $emailService->printDebugger();
 				}
 			} else {
-				session()->setFlashdata('error', 'Adresse e-mail non valide.');
+				$this->session->setFlashdata('error', 'Adresse e-mail non valide.');
 				return redirect()->to('/forgotpwd');
 			}
 		} else {
@@ -130,13 +132,13 @@ class LoginController extends BaseController {
 
 			// Vérification que le mot de passe fasse au moins 8 caractères, contienne une majuscule, une minuscule et un chiffre
 			if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', $this->request->getVar('mdp'))) {
-				session()->setFlashdata('error', 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre');
+				$this->session->setFlashdata('error', 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre');
 				return redirect()->to("/resetpwd/$token");
 			}
 
 			//Vérification du mot de passe
 			if ($this->request->getVar('mdp') != $this->request->getVar('mdp_confirm')) {
-				session()->setFlashdata('error', 'Les mots de passe ne correspondent pas');
+				$this->session->setFlashdata('error', 'Les mots de passe ne correspondent pas');
 				return redirect()->to("/resetpwd/$token");
 			}
 
@@ -144,7 +146,7 @@ class LoginController extends BaseController {
 			$utilisateur = $utilisateurModel->where('reset_token', $token)->where('reset_token_expiration >', date('Y-m-d H:i:s'))->first();
 
 			if (!$utilisateur) {
-				session()->setFlashdata('error', 'Le lien de réinitialisation est invalide ou expiré.');
+				$this->session->setFlashdata('error', 'Le lien de réinitialisation est invalide ou expiré.');
 				return redirect()->to('/login');
 			}
 
@@ -152,7 +154,7 @@ class LoginController extends BaseController {
 			$hashedPassword = password_hash($this->request->getVar('mdp'), PASSWORD_DEFAULT);
 
 			$utilisateurModel->set('mdp', $hashedPassword)->set('reset_token', null)->set('reset_token_expiration', null)->update($utilisateur['username']);
-			session()->setFlashdata('success', 'Votre mot de passe a été réinitialisé avec succès. Vous pouvez maintenant vous connecter.');
+			$this->session->setFlashdata('success', 'Votre mot de passe a été réinitialisé avec succès. Vous pouvez maintenant vous connecter.');
 			return redirect()->to('/login');
 		} else {
 			return view('resetpwd', ['token' => $token]);
@@ -160,7 +162,7 @@ class LoginController extends BaseController {
 	}
 
 	public function logout() {
-		session()->remove('utilisateur');
+		$this->session->remove('utilisateur');
 		return redirect()->to('/login');
 	}
 
